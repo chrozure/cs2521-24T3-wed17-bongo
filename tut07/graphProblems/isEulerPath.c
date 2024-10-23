db9a4d60bf0c96161fa203be5b17b1fd79931165
Harry Zhang <z5363493@ad.unsw.edu.au>
2024-10-23 16:52:11 +1100

    code and notes ready for tut07

---
+++ tut07/graphProblems/isEulerPath.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+#include "Graph.h"
+
+struct edge {
+    int v;
+    int w;
+};
+
+bool isEulerPath(Graph g, struct edge e[], int nE);
+
+
+
+bool isEulerPath(Graph g, struct edge e[], int nE) {
+    return false;
+}
